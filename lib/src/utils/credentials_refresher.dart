c50ec041cf5c170311e44a9c7ee736ec682b09db
Slawomir Chmielewski <45609322+slawomirchmielewski@users.noreply.github.com>
2024-09-06 22:17:31 +0100

    Update credentials_refresher.dart

--- lib/src/utils/credentials_refresher.dart
+++ lib/src/utils/credentials_refresher.dart
@@ -1,6 +1,4 @@
 import 'package:oauth2/oauth2.dart';
 
 Future<void> refreshCredentials(Client client) async {
-  client.credentials
-      .refresh(identifier: client.identifier, secret: client.secret);
 }
